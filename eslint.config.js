cf8801606989f3de78318b9bcda62432079e73af
dataSURGE <142424771+getdatasurge@users.noreply.github.com>
2026-01-02 17:24:03 -0500

    Fix ESLint configuration for flat config format
    - Update eslint.config.js to use installed packages
    - Add node globals for config files
    - Disable no-explicit-any rule for logger flexibility
    - Fix package.json lint script for ESLint 8

--- eslint.config.js
+++ eslint.config.js
@@ -2,27 +2,42 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  { ignores: ['dist', 'node_modules'] },
+  js.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
     },
     plugins: {
+      '@typescript-eslint': tsPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
     },
   },
-)
+  {
+    files: ['*.config.{js,ts}', 'vite.config.ts'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+]
